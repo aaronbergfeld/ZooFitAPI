e52f079cef865e2b6c864a7081e62e984d8f8920
Aaron Bergfeld <aaronbergfeld@gmail.com>
2023-01-03 21:46:16 -0500

    add profile model

---
+++ API/journal/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from rest_framework import viewsets, permissions, generics
+
+# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
