83c08d6b0e93f82fd2c32c2aa4d1c446c72064d6
Aaron Bergfeld <aaronbergfeld@gmail.com>
2023-10-02 15:00:57 -0400

    change sync-menus api token

--- sync-menus.sh
+++ sync-menus.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 for i in {0..13}
 do
-	curl --location --request POST 'ec2-44-213-37-146.compute-1.amazonaws.com:8000/fetch_menus/'$(date +%Y/%m/%d --date='today + '$i' day')/ --header 'Authorization: Token bf58eb3654743918c78f2e83af33a643e45e2fdf'
+	curl --location --request POST 'ec2-44-213-37-146.compute-1.amazonaws.com:8000/fetch_menus/'$(date +%Y/%m/%d --date='today + '$i' day')/ --header 'API-AUTH bf58eb3654743918c78f2e83af33a643e45e2fdf'
 done
